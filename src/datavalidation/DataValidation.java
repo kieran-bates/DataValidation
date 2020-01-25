e8ca1046cddbce2dcdcf97a9f182ce7d597a511d
kieran-bates <55422606+kieran-bates@users.noreply.github.com>
2020-01-25 18:14:30 -0500

    Update DataValidation.java

--- src/datavalidation/DataValidation.java
+++ src/datavalidation/DataValidation.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kieran Bates
+ * January 25, 2020
+ * This program checks to see if different types of data input are valid
  */
 
 package datavalidation;
